6ab51bb62248a9fac35576c90d80de9a297116da
Mouni Bmk <mounika10.bommakanti@gmail.com>
2018-05-13 04:49:06 -0500

    Update Head_Library.java

--- Global_Library/src/main/java/com/mounika/library/Head_Library.java
+++ Global_Library/src/main/java/com/mounika/library/Head_Library.java
@@ -3,12 +3,4 @@ package com.mounika.library;
 public interface Head_Library {
 	String getBookTitle();
 
-	/*String getAuthor();
-
-	String getEdition();
-
-	String getGenre();
-
-	String getAvailability();*/
-
 }
