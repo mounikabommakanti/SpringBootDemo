8b4b11c7ab79e753013cba9f8bf7c6db41b93414
Mouni Bmk <mounika10.bommakanti@gmail.com>
2018-05-13 04:48:32 -0500

    Update Browse_Books.java

--- Global_Library/src/main/java/com/mounika/library/Browse_Books.java
+++ Global_Library/src/main/java/com/mounika/library/Browse_Books.java
@@ -6,14 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Browse_Books {
 	public static void main(String args[]) {
-		//ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
-		//Head_Library library = (Head_Library) context.getBean("library"); //Dependency injection
+		
 		SpringApplication.run(Browse_Books.class, args);
-		/*System.out.println(getGenre());
-		System.out.println(getBookTitle());
-		System.out.println(getAuthor());
-		System.out.println(getEdition());
-		System.out.println(getAvailability());*/
+		
 	}
 
 }
